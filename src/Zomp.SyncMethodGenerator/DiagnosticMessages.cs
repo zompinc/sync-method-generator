621fecf35de26edd4af6d85e8b4c852f606e2444
Victor Irzak <victor.irzak@zomp.com>
2023-01-19 08:49:52 -0500

    More analyzer fixes

--- src/Zomp.SyncMethodGenerator/DiagnosticMessages.cs
+++ src/Zomp.SyncMethodGenerator/DiagnosticMessages.cs
@@ -4,7 +4,7 @@ internal class DiagnosticMessages
     internal static readonly DiagnosticDescriptor ExceptionOccured = new(
         id: "ZSMGEN001",
         title: "Exception occured",
-        messageFormat: "Couldn't process method file '{0}'.",
+        messageFormat: "Couldn't process method file '{0}'",
         category: "InternalError",
         DiagnosticSeverity.Error,
         isEnabledByDefault: true);
@@ -12,7 +12,7 @@ internal class DiagnosticMessages
     internal static readonly DiagnosticDescriptor ClassMustBePartial = new(
         id: "ZSMGEN002",
         title: "Mark partial",
-        messageFormat: "Class {0} must be partial to generate code.",
+        messageFormat: "Class {0} must be partial to generate code",
         category: "InternalError",
         DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
@@ -21,7 +21,7 @@ internal class DiagnosticMessages
     internal static readonly DiagnosticDescriptor CannotRetrieveSymbol = new(
         id: "ZSMGEN003",
         title: "Cannot retrieve symbol",
-        messageFormat: "Cannot retrieve symbol from type '{0}'. Exception: {1}",
+        messageFormat: "Cannot retrieve symbol from type '{0}'. Exception: {1}.",
         category: "InternalError",
         DiagnosticSeverity.Error,
         isEnabledByDefault: true);
