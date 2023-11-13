18fd8c1045b3e11b5bcecedbcf0436d6ecfcf98f
Victor Irzak <victor.irzak@zomp.com>
2023-11-12 20:12:27 -0500

    Fix #16 Put parentheses around interpolated string

---
+++ tests/Generator.Tests/InterpolatedStringTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Generator.Tests;
+
+[UsesVerify]
+public class InterpolatedStringTests
+{
+    [Theory]
+    [InlineData("await File.ReadAllTextAsync(\"123\")")]
+    [InlineData("(await File.ReadAllTextAsync(\"123\"))")]
+    public Task EnsureParentheses(string call) => $$"""
+var z = $"123{{{call}}}456";
+""".Verify(false, true, sourceType: SourceType.MethodBody);
+
+    [Theory]
+    [InlineData("await File.ReadAllTextAsync(\"123\")")]
+    [InlineData("(await File.ReadAllTextAsync(\"123\"))")]
+    public Task HandleFormatString(string call) => $$"""
+var z = $"123{{{call}}:hh}456";
+""".Verify(false, true, sourceType: SourceType.MethodBody);
+}
---
+++ tests/Generator.Tests/Snapshots/InterpolatedStringTests.EnsureParentheses#g.verified.cs
@@ -0,0 +1,2 @@
+﻿//HintName: Test.Class.MethodAsync.g.cs
+var z = $"123{(global::System.IO.File.ReadAllText("123"))}456";
