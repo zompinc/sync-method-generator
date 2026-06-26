3fcb4e0930979b61d50fa5f6afcec0fd485b2dd5
Michele Bastione <michele.bastione@gmail.com>
2026-06-26 16:20:28 +0200

    Fixed regression with async LINQ methods not being converted to their respective synchronous ones (#123)

---
+++ tests/Generator.Tests/Snapshots/SystemAsyncExtensionsTests.LinqConversion#g.verified.cs
@@ -0,0 +1,5 @@
+﻿//HintName: Test.Class.MethodAsync.g.cs
+public void Method(global::System.Collections.Generic.IEnumerable<int> foo)
+{
+    var elements = global::System.Linq.Enumerable.ToList(global::System.Linq.Enumerable.Where(global::System.Linq.Enumerable.Select(foo, i => i + 5), i => i > 0));
+}
